e05b0e0deb7e34e73531d16ef1d28dc32bb93ff8
k-haripriya <haripriyakarthikeyan003@gmail.com>
2024-06-03 11:36:10 +0530

    Routes initialization started

---
+++ src/Routes/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import StackNavigator from './StackNavigator'
+
+const Routes = () => {
+  return (
+    <StackNavigator/>
+  )
+}
+
+export default Routes
